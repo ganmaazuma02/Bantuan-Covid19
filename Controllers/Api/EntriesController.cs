293430638a5f4e6cdf89edc78f0bc8571c7b1a88
anas_amir_nur_rashid <anas_amir_nur_rashid@dell.com>
2020-04-03 19:20:30 +0800

    Add project files.

---
+++ Controllers/Api/EntriesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BorangBantuanCovid19.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntriesController : ControllerBase
+    {
+        [HttpGet("api/[controller]")]
+        public async Task<ActionResult> GetAll()
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/EntriesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BorangBantuanCovid19.Controllers
+{
+    public class EntriesController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+    }
+}
